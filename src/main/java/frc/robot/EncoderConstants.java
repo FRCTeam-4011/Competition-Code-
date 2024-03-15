06c6c8e745a9dd54b51b3f935d1a55d6ccf7035d
BJ Gerling <107522124+vkskoll@users.noreply.github.com>
2024-03-14 19:21:48 -0500

    Code with minimal pathplanner as well as climbing

---
+++ src/main/java/frc/robot/EncoderConstants.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class EncoderConstants {
+    public static final double ek_IntakeArmPOS = 0.0;
+    public static final double ek_PodiumShotArmPOS = -9.50;
+    public static final double ek_BackPodiumShotArmPOS = -6.033;
+    public static final double ek_AmpShotArmPOS = -19.34;
+    public static final double ek_SubShotArmPOS = -19.34;// change to -19.04
+    public static final double ek_HomeArmPOS = -0.10;
+    public static final double ek_CustomArmPOS = 0.0;
+    public static final double ek_LeftClimberMaxUp = 0.0;
+    public static final double ek_RightClimberMaxUp = 0.0;
+    public static final double ek_LeftClimberMaxDown = 0.0;
+    public static final double ek_RightClimberMaxDown = 0.0;
+
+}
